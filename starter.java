2ca4a805d8b8c99253ce7cddb33a4237b86e773f
B. Ricks <bricks@unomaha.edu>
2021-07-23 13:42:21 -0500

    Comment the java code

--- starter.java
+++ starter.java
@@ -1,47 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 
-/**
- *
- * @author unouser
- */
 public class starter {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-        //Constructor: width, height, pixel format
-        try {
-        
-        BufferedImage bufferedImage = ImageIO.read(new File("bridge.jpg"));
-        //BufferedImage bufferedImage = new BufferedImage(200, 200, BufferedImage.TYPE_INT_ARGB);
-        
-        for(int i =0; i < 200; i++)
-        {
-            bufferedImage.setRGB(i, i, Color.BLUE.getRGB());
-            
+  public static void main(String[] args) {
+
+    try {
+
+      BufferedImage bufferedImage = ImageIO.read(new File("bridge.jpg"));
+
+      // To create an image from scratch, use the following
+      // BufferedImage bufferedImage = new BufferedImage(200, 200,
+      // BufferedImage.TYPE_INT_ARGB);
+
+
+      //Loop over every pixel
+      for (int y = 0; y < bufferedImage.getHeight(); y++) {
+        for (int x = 0; x < bufferedImage.getWidth(); x++) {
+          int pixelInt = bufferedImage.getRGB(x, y);
+          Color pixelColor = new Color(pixelInt);
+
+          //Just do a grayscale conversion for half the image
+          if (x < bufferedImage.getWidth() / 2) {
+            //Get the int [0,255] values for red, green, and blue
+            int r = pixelColor.getRed();
+            int g = pixelColor.getGreen();
+            int b = pixelColor.getBlue();
+
+            //Do a trivial grayscale conversion by just using the red channel.
+            g = r;
+            b = r;
+
+            //Create a new color object with the new colors
+            Color outColor = new Color(r, g, b);
+
+            //Set the pixel to the color
+            bufferedImage.setRGB(x, y, outColor.getRGB());
+          }
         }
-        
-        
-            ImageIO.write(bufferedImage, "PNG", new File("out.png"));
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-        
-        
+      }
+
+      //Draw a red rectangle on the image
+
+      //Get the graphics context
+      Graphics g = bufferedImage.getGraphics();
+
+      //Draw the rectangle
+      g.setColor(Color.RED);
+      g.drawRect(10, 10, 100, 100);
+
+      // Save the image to a file
+      ImageIO.write(bufferedImage, "PNG", new File("out.png"));
+
+    } catch (IOException ex) {
+      ex.printStackTrace();
     }
-    
+  }
 }
